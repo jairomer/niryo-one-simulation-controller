5205f69ae5d10b606f299136f29f7935c25a8513
jairmer <jairomer@protonmail.com>
2020-07-15 00:29:38 +0200

    README update

--- README.md
+++ README.md
@@ -9,4 +9,12 @@ clone in catkin_ws/src.
 
 ## Run it
 
-`rosrun simulation_controller simulation_controller_node`
+0. Prepare for current setup. 
+ - Configuration for the environment is contained in the docker run command on `run_container.sh`
+ - All relevant hosts on the ROS network should be added as hosts. 
+ - Set the ROS master uri to the one on your setup.
+ - Set the ROS ip to the one of the interface connecting to the ROS network.
+
+1. Execute script to setup ROS environment as a docker container and start node.
+    `./run_container`
+
--- run_container.sh
+++ run_container.sh
@@ -2,19 +2,18 @@
 sudo docker build . -t ros_controller
 
 # Networking settings
-ROS_MASTER_URI="http://10.10.10.10:11311"
-ROS_IP="10.10.10.123"
+ROS_MASTER_URI="http://192.168.0.181:11311"
+ROS_IP="192.168.0.199"
 sudo docker run \
 	--hostname controller \
 	-it \
 	--rm \
 	--net=host \
 	-e ROS_MASTER_URI=$ROS_MASTER_URI \
-        -e ROS_IP=$ROS_IP \
+    -e ROS_IP=$ROS_IP \
 	--add-host controller:127.0.0.1 \
-	--add-host coppeliaSim:10.10.10.123 \
-	--add-host niryo-desktop:10.10.10.10 \
+	--add-host coppeliaSim:192.168.0.199 \
+	--add-host ros:192.168.0.181 \
 	ros_controller:latest \
-	bash
-  	
+#  	bash
 #	--user root \
